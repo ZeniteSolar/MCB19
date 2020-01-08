66b40e693583cd86f1ff5664bb375846639f46aa
João Antônio Cardoso <joao.maker@gmail.com>
2020-01-08 17:15:09 -0300

    cleaning

--- firmware/makelibs.sh
+++ firmware/makelibs.sh
@@ -1,7 +1,4 @@
-cd lib/avr-can-lib
-git submodule init
-git submodule update
-cd src/
+cd lib/avr-can-lib/src
 make clean
 make lib
 cd ../../
--- firmware/up.sh
+++ firmware/up.sh
@@ -1,8 +1,8 @@
-
+#!/bin/sh
 #cppcheck  --force --std=c99 --language=c src/
 make clean
 rm -rf bin obj
 make all
 #fuser -k /dev/ttyACM0
-avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
-figlet "MSC19"
+avrdude -v -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
+figlet "MCB19"
