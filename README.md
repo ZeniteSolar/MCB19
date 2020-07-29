d95a805865793b89bf27dd56a0fe81ad6b9f0f7b
João Antônio Cardoso <joao.maker@gmail.com>
2020-07-29 16:10:21 -0300

    Fix Build for github actions (#6)
    * Use generic avr for cross
    
    * Stop when one of commands not succed
    
    * Create an artifact of the build
    
    * Fix compiler flags
    
    * Show avr-gcc version
    
    * Add BUILD badge

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MCB19
 
+![BUILD](https://github.com/ZeniteSolar/MCB19/workflows/BUILD/badge.svg?branch=master)
+
 **M**ódulo **C**arregamento das **B**aterias 2019 
 
 Embarcação Guarapuvu II
--- firmware/makelibs.sh
+++ firmware/makelibs.sh
@@ -1,5 +1,5 @@
-cd lib/avr-can-lib/src/
-make clean
-make lib
-cd ../../../
+cd lib/avr-can-lib/src/ &&
+make clean &&
+make lib &&
+cd ../../../ &&
 curl https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/master/can_ids.h -o src/can_ids.h
