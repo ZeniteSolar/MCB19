c9e4ae6325b5eb67297a5caf20ba73f5441094ec
João Antônio Cardoso <joao.maker@gmail.com>
2020-08-19 18:19:11 -0300

    Generic figlet message and some cleaning

--- firmware/up.sh
+++ firmware/up.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-#cppcheck  --force --std=c99 --language=c src/
 make clean
 rm -rf bin obj
 make all
 #fuser -k /dev/ttyACM0
 avrdude -v -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
-figlet "MCB19"
+figlet $(basename $(realpath ../))
